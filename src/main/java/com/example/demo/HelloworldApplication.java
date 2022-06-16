c7e80b99cb24a150c34a13dbf1e598b6bada4f03
a.labate <a.labate@elis.org>
2022-06-16 17:07:15 +0200

    2 dev

--- src/main/java/com/example/demo/HelloworldApplication.java
+++ src/main/java/com/example/demo/HelloworldApplication.java
@@ -10,6 +10,7 @@ public class HelloworldApplication {
 		SpringApplication.run(HelloworldApplication.class, args);
 		System.out.println("1");
 		System.out.println("2");
+		System.out.println("dev");
 	}
 
 }
